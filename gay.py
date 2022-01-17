6bccbf41e9305ebb719ab162bd4ce1df34d90a4e
Md Noor <75603153+Mdnoor786@users.noreply.github.com>
2022-01-17 08:55:32 +0530

    Create gay.py

---
+++ gay.py
@@ -0,0 +1,14 @@
+import logging
+from os import system
+
+# Logs
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
+)
+# Starting bot
+system("git clone https://gitHub.com/DEADLY-FIGHTERS/DEADLY-SPAM-BOT && cd DEADLY-SPAM-BOT && python3 -m OfficialSameer")
+# if got error
+print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @LionXsupport")
+# Script written for stop heroku suspension
+# use with credit else gay
+#By @MdNoor786 (t.me/Md_Noor786) For Fixing Heroku Suspension
