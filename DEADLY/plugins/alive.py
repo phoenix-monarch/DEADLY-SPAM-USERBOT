c90853c6cb8864cf5622475eaf27ae7f770d3930
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-11-07 09:08:58 +0530

    H

--- DEADLY/plugins/alive.py
+++ DEADLY/plugins/alive.py
@@ -1,1 +1,12 @@
+from pyrogram import filters, Client
+from pyrogram.types import *
 
+@Client.on_message(filters.command(["alive", "start"],["!", "/", ",", ".") & filters.SUDOERS)
+async def alive(client: Client, message: Message):
+       alive = """
+A
+B
+C
+"""
+       pics = random.choice(PICS) 
+       await message.reply_photo(photo=pics, caption=alive) 
