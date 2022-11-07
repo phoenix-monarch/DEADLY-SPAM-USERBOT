74507c5056af711d7335efe4a1ce60cffbff8e28
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-11-07 08:39:44 +0530

    Update join.py

--- DEADLY/plugins/join.py
+++ DEADLY/plugins/join.py
@@ -1,1 +1,36 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
+@Client.on_message(filters.command("join", ".") & filters.me)
+async def join(client: Client, message: Message):
+    blaze = message.text[6:]
+    count = 0
+    if not blaze:
+        return await message.reply_text("Need a chat username or invite link to join.")
+    if blaze.isnumeric():
+        return await message.reply_text("Can't join a chat with chat id. Give username or invite link.")
+    try:
+        await client.join_chat(blaze)
+        await message.reply_text(f"**Spammers Successfully Joined The Chat 🥱**")
+    except Exception as ex:
+        await message.reply_text(f"**ERROR:** \n\n{str(ex)}")
+
+@Client.on_message(filters.command("leave", ".") & filters.me)
+async def leave(client: Client, message: Message):
+    blaze = message.text[6:]
+    count = 0
+    if not blaze:
+        return await message.reply_text("Need a chat username or invite link to leave.")
+    if blaze.isnumeric():
+        return await message.reply_text("Can't leave a chat with chat id. Give username or invite link.")
+    try:        
+        await message.reply_text(f"**Spammers Successfully Left The Chat**")
+        await client.leave_chat(blaze)
+    except Exception as ex:
+        await message.reply_text(f"**ERROR:** \n\n{str(ex)}")
+
+joinhelp = """
+
+!join @username or https://t.me/abc
+!leave @username or https://t.me/abc
+"""
