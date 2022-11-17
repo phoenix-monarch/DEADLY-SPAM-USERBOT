2d48c51afeeeefa80db5ec2d8e59820696f0ed77
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-11-17 20:21:15 +0530

    Update __init__.py

--- DEADLY/__init__.py
+++ DEADLY/__init__.py
@@ -1,1 +1,11 @@
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
 
+API_ID = API_ID
+API_HASH = API_HASH
+LOG_GRP = LOG_GRP
+OWNER_ID = OWNER_ID
+ALIVE_PIC = ALIVE_PIC
+
+userbot = {}
