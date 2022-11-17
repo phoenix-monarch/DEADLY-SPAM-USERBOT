71d04e5e3f9269b7bb268347e8f43bfddc9faac4
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-11-17 20:30:37 +0530

    Update dm.py

--- DEADLY/plugins/dm.py
+++ DEADLY/plugins/dm.py
@@ -5,17 +5,21 @@ from DEADLY import *
 
 @Client.on_message(filters.user(SUDOERS) & filters.command(["dm", "ib"], [".", "!", "/"]))
 async def dmm(client: Client, message: Message):
-    blaze = await message.reply_text("⚡ 𝐇𝐄𝐋𝐏𝐋𝐈𝐍𝐄:\n .dm @username <yourmsz>")
+    blaze = await message.reply_text("⚡ 𝐇𝐄𝐋𝐏𝐋𝐈𝐍𝐄:\n .dm @username <yourmsz> or reply to user")
     quantity = 1
     inp = message.text.split(None, 2)[1]
     user = await client.get_chat(inp)
     spam_text = ' '.join(message.command[2:])
     quantity = int(quantity)
-
+    
+    if int(message.user.id) in DEV:
+       await blaze.edit("<b>Bhsdk 😂 Creator hai wo is source ka usko pelega tu? </b>") 
+        return
+ 
     if message.reply_to_message:
         reply_to_id = message.reply_to_message.message_id
         for _ in range(quantity):
-            await blaze.edit("Successfully Sent Dm 🥳")
+            await blaze.edit("**Inbox Sent Successfully 🌚**")
             await client.send_message(user.id, spam_text,
                                       reply_to_message_id=reply_to_id)
             await asyncio.sleep(0.15)
@@ -23,6 +27,6 @@ async def dmm(client: Client, message: Message):
 
     for _ in range(quantity):
         await client.send_message(user.id, spam_text)
-        await blaze.edit("Message Sended Successfully ✅")
+        await blaze.edit("Message Delivered Successfully ✅")
         await asyncio.sleep(0.15)
 
