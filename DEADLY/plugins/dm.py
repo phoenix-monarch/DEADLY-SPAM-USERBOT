96839b5e11877f46333ea4aa4566b2e662f70116
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-11-18 15:54:11 +0530

    Update dm.py

--- DEADLY/plugins/dm.py
+++ DEADLY/plugins/dm.py
@@ -18,7 +18,7 @@ Sender must be SUDOERS .
 
 @Client.on_message(filters.user(SUDOERS) & filters.command(["dm", "ib"], [".", "!", "/"]))
 async def dmm(client: Client, message: Message):
-    blaze = await message.reply_text(DM_TXT)
+    blaze = await message.reply_text("Processing...")
     quantity = 1
     inp = message.text.split(None, 2)[1]
     user = await client.get_chat(inp)
@@ -39,6 +39,36 @@ async def dmm(client: Client, message: Message):
         return
 
     for _ in range(quantity):
+        await client.send_message(user.id, spam_text)
+        await blaze.edit("Message Delivered Successfully ✅")
+        await asyncio.sleep(0.15)
+
+
+
+@Client.on_message(filters.user(SUDOERS) & filters.command(["draid", "inraid"], [".", "!", "/"]))
+async def dmm(client: Client, message: Message):
+    blaze = await message.reply_text("Processing...")
+    quantity = 1
+    inp = message.text.split(None, 2)[1]
+    user = await client.get_chat(inp)
+    quantity = int(quantity)
+    
+    if int(user.id) in DEV:
+       await blaze.edit("<b>Bhsdk 😂 Creator hai wo is source ka usko pelega tu? </b>") 
+       return
+ 
+    if message.reply_to_message:
+        reply_to_id = message.reply_to_message.message_id
+        spam_text = random.choice(RAID) 
+        for _ in range(quantity):
+            await blaze.edit("**Inbox Sent Successfully 🌚**")
+            await client.send_message(user.id, spam_text,
+                                      reply_to_message_id=reply_to_id)
+            await asyncio.sleep(0.15)
+        return
+
+    for _ in range(quantity):
+        spam_text = random.choice(RAID) 
         await client.send_message(user.id, spam_text)
         await blaze.edit("Message Delivered Successfully ✅")
         await asyncio.sleep(0.15)
