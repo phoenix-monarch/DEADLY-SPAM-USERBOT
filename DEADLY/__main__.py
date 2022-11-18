b047c2c432ad6147b70f76245f0cedd09a1b129a
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-11-18 15:59:12 +0530

    Update __main__.py

--- DEADLY/__main__.py
+++ DEADLY/__main__.py
@@ -1,8 +1,11 @@
 import config
 import asyncio
 from pyrogram import idle
-from DEADLY import bot1, bot2, bot3, bot4, bot5, bot6, bot7, bot8, bot9, bot10, OWNER_ID
+from DEADLY import bot1, bot2, bot3, bot4, bot5, bot6, bot7, bot8, bot9, bot10
 
+
+LOG = "DEADLY_SPAM_BOT"
+OWNER_ID = LOG
 
 async def main():
     await bot1.start() 
