84b95de79af8fc8cf0f6ec0e7e5b5e6bb19f7374
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-11-18 14:36:39 +0530

    Update alive.py

--- DEADLY/plugins/alive.py
+++ DEADLY/plugins/alive.py
@@ -12,6 +12,30 @@ from resources.data import PROGROUPS, DEV
 From .import *
 from DEADLY import ALIVE_PIC, SUDOERS
 
+
+
+START_TIME = datetime.utcnow()
+TIME_DURATION_UNITS = (
+    ('Week', 60 * 60 * 24 * 7),
+    ('Day', 60 * 60 * 24),
+    ('Hour', 60 * 60),
+    ('Min', 60),
+    ('Sec', 1)
+)
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+
+
+
+
 pthversion = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
 
 
