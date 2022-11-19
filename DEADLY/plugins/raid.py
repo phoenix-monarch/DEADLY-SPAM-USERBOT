3344ee64ec60a2bfb02644aca52b64f90d86b18b
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-11-19 10:43:39 +0530

    Update raid.py

--- DEADLY/plugins/raid.py
+++ DEADLY/plugins/raid.py
@@ -1,10 +1,14 @@
 import asyncio
 import random
 import asyncio
+import time
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from pyrogram import filters, Client
 from resources.data import RAID, PROGROUPS, DEV
+from traceback import format_exc
+from typing import Tuple
 from .sudo import SUDOERS
 
 @Client.on_message(filters.user(SUDOERS) & filters.command(["raid"], [".", "!", "/"]))
@@ -20,6 +24,7 @@ async def raid(client: Client, message: Message):
             return
     userz = await client.get_users(user)
     quantity = message.command[1]
+    failed = 0 
     quantity = int(quantity)
     if int(message.chat.id) in PROGROUPS:
         await sex.edit("**Sorry You Cannot Spam In Deadly Chats!**")
@@ -126,7 +131,7 @@ HELP_TXT = """
 """
 
 
-@Client.on_message(filters.user(SUDOERS) & filters.command(["help", "cmds"], [".", "!", "/"]))
+@Client.on_message(filters.user(SUDOERS) & filters.command(["cmds"], [".", "!", "/"]))
 async def hsk(client: Client, message: Message):
     quantity = 1
     sex = await message.reply_text("`Processing..`")
