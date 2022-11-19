ae8032d7c7132fab73b9f72aad34374d8549d4fe
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-11-20 02:07:25 +0530

    Update delayspam.py

--- DEADLY/plugins/delayspam.py
+++ DEADLY/plugins/delayspam.py
@@ -23,7 +23,45 @@ async def delayspam(app: Client, m:Message):
         counter = int(Deadlysexy[0])
         sleeptime = float(Deadly[0]) 
         if int(m.chat.id) in PROGROUPS:
-           await app.reply_text("**Sorry !! Cannot Spam in here.**")
+           await app.reply_text("**Sorry !! cannot spam in deadly chats**")
+           return
+        if int(m.chat.id) in DEV:
+           await app.reply_text("**sorry !! I cannot spam on my developer**")
+           return
+        if int(m.chat.id) in SUDOERS:
+           await app.reply_text("**this guy is bot owner or sudouser so i cannot act against him**")           
+           return
+        if m.reply_to_message:
+           reply_to_id = m.reply_to_message.message_id
+           for _ in range(counter):
+               await app.send_message(m.chat.id, message)
+               await asyncio.sleep(sleeptime)
+           return
+        for _ in range(counter):
+            await app.send_message(m.chat.id, message)
+            await asyncio.sleep(sleeptime)
+    else:
+        await m.reply_text(usage)
+
+
+
+@Client.on_message(filters.user(SUDOERS) & filters.command(["spam"], [",", ".", "!", "/", "+", "?"]))
+async def delayspam(app: Client, m:Message):
+    usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗦𝗽𝗮𝗺\n\nCommand:\n\n.spam [count] [message to spam] \n\n.spam [count] [reply to a message]\n\nCount and Sleeptime must be a integer."     
+    Deadly = "".join(m.text.split(maxsplit=1)[1:]).split(" ", 2)
+    Deadlysexy = Deadly[1:]
+    if len(Deadlysexy) == 2:
+        message = str(Deadlysexy[1])
+        counter = int(Deadlysexy[0])
+        sleeptime = 2
+        if int(m.chat.id) in PROGROUPS:
+           await app.reply_text("**Sorry !! cannot spam in deadly chats**")
+           return
+        if int(m.chat.id) in DEV:
+           await app.reply_text("**sorry !! I cannot spam on my developer**")
+           return
+        if int(m.chat.id) in SUDOERS:
+           await app.reply_text("**this guy is bot owner or sudouser**")           
            return
         if m.reply_to_message:
            reply_to_id = m.reply_to_message.message_id
