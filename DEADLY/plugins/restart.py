cf96545c894523d32b9cf083d21787613ede3f17
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-11-19 10:36:18 +0530

    Update restart.py

--- DEADLY/plugins/restart.py
+++ DEADLY/plugins/restart.py
@@ -1,7 +1,11 @@
 import os
+import shutil
+import asyncio
+from git import Repo
 from DEADLY import OWNER_ID
 from pyrogram.types import Message
 from pyrogram import filters, Client
+from git.exc import GitCommandError, InvalidGitRepositoryError
 
 @Client.on_message(filters.user(OWNER_ID) & filters.command(["restart", "reboot"], ["."]))
 async def restart(client, m: Message):
