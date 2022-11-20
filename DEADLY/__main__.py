ed7f3d19bffd2667b6360b7429ee37c9d9a64478
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-11-20 11:41:44 +0530

    Update __main__.py

--- DEADLY/__main__.py
+++ DEADLY/__main__.py
@@ -7,87 +7,35 @@ from DEADLY import bot1, bot2, bot3, bot4, bot5, bot6, bot7, bot8, bot9, bot10
 OWNER_ID = "ELRIC_XD"
 
 if bot1:
-   bot1.start() 
-   bot1.join_chat("Deadly_spam_bot") 
-   bot1.join_chat("deadly_spambot") 
-   bot1.send_message(
-            OWNER_ID, 
-            "<b> Congrats!! DeadlySpam [1] Started Successfully!</b>", 
-        ) 
+    bot1.start()
+    bot1.join_chat("deadly_spambot")
 if bot2:
-   bot2.start() 
-   bot2.join_chat("Deadly_spam_bot") 
-   bot2.join_chat("deadly_spambot") 
-   bot2.send_message(
-               OWNER_ID, 
-               "<b> Congrats!! DeadlySpam [2] Started Successfully!</b>", 
-           )
+    bot2.start()
+    bot2.join_chat("deadly_spambot")
 if bot3:
-   bot3.start() 
-   bot3.join_chat("Deadly_spam_bot") 
-   bot3.join_chat("deadly_spambot") 
-   bot3.send_message(
-               OWNER_ID, 
-               "<b> Congrats!! DeadlySpam [3] Started Successfully!</b>", 
-           )
+    bot3.start()
+    bot3.join_chat("deadly_spambot")
 if bot4:
-   bot4.start() 
-   bot4.join_chat("Deadly_spam_bot") 
-   bot4.join_chat("deadly_spambot") 
-   bot4.send_message(
-               OWNER_ID, 
-               "<b> Congrats!! DeadlySpam [4] Started Successfully!</b>", 
-               )
+    bot4.start()
+    bot4.join_chat("deadly_spambot")
 if bot5:
-   bot5.start() 
-   bot5.join_chat("Deadly_spam_bot") 
-   bot5.join_chat("deadly_spambot") 
-   bot5.send_message(
-               OWNER_ID, 
-               "<b> Congrats!! DeadlySpam [5] Started Successfully!</b>", 
-           )
+    bot5.start()
+    bot5.join_chat("deadly_spambot")
 if bot6:
-   bot6.start() 
-   bot6.join_chat("Deadly_spam_bot") 
-   bot6.join_chat("deadly_spambot") 
-   bot6.send_message(
-               OWNER_ID, 
-               "<b> Congrats!! DeadlySpam [6] Started Successfully!</b>", 
-           )
+    bot6.start()
+    bot6.join_chat("deadly_spambot")
 if bot7:
-   bot7.start() 
-   bot7.join_chat("Deadly_spam_bot") 
-   bot7.join_chat("deadly_spambot") 
-   bot7.send_message(
-               OWNER_ID, 
-               "<b> Congrats!! DeadlySpam [7] Started Successfully!</b>", 
-           )
+    bot7.start()
+    bot7.join_chat("deadly_spambot")
 if bot8:
-   bot8.start() 
-   bot8.join_chat("Deadly_spam_bot") 
-   bot8.join_chat("deadly_spambot") 
-   bot8.send_message(
-               OWNER_ID, 
-               "<b> Congrats!! DeadlySpam [8] Started Successfully!</b>", 
-           )
+    bot8.start()
+    bot8.join_chat("deadly_spambot")
 if bot9:
-   bot9.start() 
-   bot9.join_chat("Deadly_spam_bot") 
-   bot9.join_chat("deadly_spambot") 
-   bot9.send_message(
-               OWNER_ID, 
-               "<b> Congrats!! DeadlySpam [9] Started Successfully!</b>", 
-           )
+    bot9.start()
+    bot9.join_chat("deadly_spambot")
 if bot10:
-   bot10.start() 
-   bot10.join_chat("Deadly_spam_bot") 
-   bot10.join_chat("deadly_spambot") 
-   bot10.send_message(
-               OWNER_ID, 
-               "<b> Congrats!! DeadlySpam [10] Started Successfully!</b>", 
-           )
-
-
+    bot10.start()
+    bot10.join_chat("deadly_spambot")
 
 print("Your DeadlySpam Userbot Deployed Successfully 🥳 Please visit @deadly_spam_bot for any help!") 
 
