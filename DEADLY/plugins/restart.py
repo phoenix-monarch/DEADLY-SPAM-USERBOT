5c48f682c2418735996e67a201e5b6c6a5a65919
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-11-20 11:00:13 +0530

    Update restart.py

--- DEADLY/plugins/restart.py
+++ DEADLY/plugins/restart.py
@@ -7,11 +7,18 @@ from pyrogram.types import Message
 from pyrogram import filters, Client
 from git.exc import GitCommandError, InvalidGitRepositoryError
 
+
+
 @Client.on_message(filters.user(SUDOERS) & filters.command(["restart", "reboot"], ["."]))
 async def restart(client, m: Message):
     reply = await m.reply_text("Restarting....")     
     await m.delete() 
-    await reply.edit(
-        "<b>Reboot has been initiated successfully! Wait for 1 - 2 minutes until the bot restarts.</b>"
-    )
-    os.system(f"kill -9 {os.getpid()} && bash start")
+    retard = "<b>Reboot has been initiated successfully! Wait for 1 - 2 minutes until the bot restarts.</b>"
+    await m.edit(retard) 
+    try:
+        client.disconnect()
+    except Exception as e:
+         pass
+    args = [sys.executable, "-m", "DEADLY"]
+    os.execl(sys.executable, *args)
+    quit()
